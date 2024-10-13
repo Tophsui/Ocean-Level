0cc58aa68da608b0c975a6c5205ee8e3aaafa196
Toph <revertzlol@gmail.com>
2024-10-13 15:25:48 -0400

    Added user input for ocean level rise per year

--- Ocean Level.cpp
+++ Ocean Level.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 
 int main() {
-    // Constant for ocean level rise per year (in millimeters)
-    const double OCEAN_RISE_PER_YEAR = 1.5;
+    // Variable for user input
+    double oceanRisePerYear;
+
+    // Get input from user
+    cout << "Enter the number of millimeters the ocean's level rises per year: ";
+    cin >> oceanRisePerYear;
 
     // Calculating ocean level in 5, 7, and 10 years
-    double levelIn5Years = OCEAN_RISE_PER_YEAR * 5;
-    double levelIn7Years = OCEAN_RISE_PER_YEAR * 7;
-    double levelIn10Years = OCEAN_RISE_PER_YEAR * 10;
+    double levelIn5Years = oceanRisePerYear * 5;
+    double levelIn7Years = oceanRisePerYear * 7;
+    double levelIn10Years = oceanRisePerYear * 10;
 
     // Display the results
     cout << "The ocean's level will be " << levelIn5Years << " mm higher in 5 years." << endl;
